6d23f6073dccd374e826192b3b2765d3fc58451f
abhigya-sodani <37461794+abhigya-sodani@users.noreply.github.com>
2020-10-13 19:59:41 -0700

    Add files via upload

---
+++ gibDown.py
@@ -0,0 +1,57 @@
+from annoy import AnnoyIndex
+import requests
+from io import BytesIO
+import cv2
+from PIL import Image
+import numpy as np
+import time
+import numpy as np
+from numpy.linalg import norm
+import pickle
+from tqdm import tqdm, tqdm_notebook
+import os
+import random
+import time
+import math
+import requests
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.applications.vgg19 import VGG19
+from tensorflow.keras.applications.mobilenet import MobileNet
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Flatten, Dense, Dropout, GlobalAveragePooling2D
+
+def extract_features(url,name):
+        response = requests.get(url)
+        print(url)
+        img = Image.open(BytesIO(response.content))
+        a=np.asarray(img)
+        a=np.resize(a,(512,512,3))
+        cv2.imwrite(name+".jpg",a)
+
+counter=0
+for i in range(0,160):
+        for j in range(0,320):
+           
+            if(counter==199):
+                    break
+                    
+            with open("C://Users//abhig//level8images//"+str(counter)+'.jpg', 'wb') as handle:
+                    response = requests.get("https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/MODIS_Terra_CorrectedReflectance_TrueColor/default/2005-08-29/250m/8/"+str(i)+"/"+str(j)+".jpg",stream=True)
+
+                    if not response.ok:
+                        print(response)
+
+                    for block in response.iter_content(1024):
+                        if not block:
+                            break
+
+                        handle.write(block)
+            print(str(i),str(j))
+                       
+            counter+=1
+
