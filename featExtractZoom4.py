f6bee2ccb26bf08480bc05c22ee8c194a275027c
abhigya-sodani <37461794+abhigya-sodani@users.noreply.github.com>
2020-11-01 21:37:42 -0800

    Update featExtractZoom4.py

--- featExtractZoom4.py
+++ featExtractZoom4.py
@@ -25,48 +25,53 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Flatten, Dense, Dropout, GlobalAveragePooling2D
 import wget
 
+def extract_features(url, model):
+        
+       
+  response = wget.download(url)
+  a = cv2.imread(response)
+  os.remove(response)
+  a=np.resize(a,(1,224,224,3))
+  a=a/255       
+  #preprocessed_img = preprocess_input(a)
+  features = model.predict(a)
+  flattened_features = features.flatten()
+  normalized_features = flattened_features / norm(flattened_features)
+  return normalized_features
+
+def extract_model_till_layer(model, layerNo):
+  
+  outputs = model.layers[layerNo].output
+  model = Model(inputs=model.inputs, outputs=outputs)
+  return model
+
+
 model2 = ResNet50(weights='imagenet',
                          include_top=False,
                          input_shape=(512, 512, 3),
                         pooling='max')
-model = tf.keras.models.load_model('best_worldview_2.h5')
+model = tf.keras.models.load_model('best_worldview_2.h5') #replace with whichever model you wish to featurize with
 model.summary()
 
-def extract_model_till_layer(model, layerNo):
-  outputs = model.layers[layerNo].output
-  model = Model(inputs=model.inputs, outputs=outputs)
-  return model
-model1=extract_model_till_layer(model,10)
+
+model1=extract_model_till_layer(model,10) #change for what layer you want to featurize till
                       
 features=[]
 try:
     
-    baseUrl="https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/MODIS_Terra_CorrectedReflectance_TrueColor/default/2012-07-09/250m/8/29/0.jpg"
-    def extract_features(url, model):
-        
-       
-        response = wget.download(url)
-        a = cv2.imread(response)
-        os.remove(response)
-        a=np.resize(a,(1,224,224,3))
-        a=a/255       
-        #preprocessed_img = preprocess_input(a)
-        features = model.predict(a)
-        flattened_features = features.flatten()
-        normalized_features = flattened_features / norm(flattened_features)
-        return normalized_features
+   
+   
 
 
     for i in range(0,10):
         for j in range(0,20):
-            a=extract_features("https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/MODIS_Terra_CorrectedReflectance_TrueColor/default/2005-08-29/250m/4/"+str(i)+"/"+str(j)+".jpg", model1)
-            print("length",len(a))
-            features.append(a)
+            feats=extract_features("https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/MODIS_Terra_CorrectedReflectance_TrueColor/default/2005-08-29/250m/4/"+str(i)+"/"+str(j)+".jpg", model1)
+            print("length",len(feats))
+            features.append(feats)
             print(str(i)+" "+str(j))
             
         time.sleep(30)
-    print(len(features[0]))
-    print(features[0])
+
 
 
      # Length of item vector that will be indexed
@@ -75,15 +80,15 @@ try:
         feature = features[p]
         t.add_item(p, feature)
 
-    t.build(40)  # 50 trees
+    t.build(40)  # 40 trees
     t.save('hurricanes1.ann')
 
 except:
-    print("aaaa")
+    print("Error occured, indexing the current features")
     t=AnnoyIndex(features[0])
     for p in range(len(features)):
         feature = features[p]
         t.add_item(p, feature)
 
-    t.build(40)  # 50 trees
+    t.build(40)  # 40 trees
     t.save('hurricanes1.ann')
