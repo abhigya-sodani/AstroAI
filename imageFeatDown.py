e12ae968c57c69506abb59978cc12686b34a2f70
abhigya-sodani <37461794+abhigya-sodani@users.noreply.github.com>
2020-11-01 21:53:51 -0800

    Update imageFeatDown.py

--- imageFeatDown.py
+++ imageFeatDown.py
@@ -26,6 +26,23 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Flatten, Dense, Dropout, GlobalAveragePooling2D
 import wget
 
+def extract_model_till_layer(model, layerNo):
+  
+  outputs = model.layers[layerNo].output
+  model = Model(inputs=model.inputs, outputs=outputs)
+  return model
+
+def extract_features(file, model):
+        
+  a = np.asarray(PIL.Image.open('C://Users//abhig//level8images//'+file))
+  a=np.resize(a,(1,224,224,3))
+  a=a/255       
+  #preprocessed_img = preprocess_input(a)
+  features = model.predict(a)
+  flattened_features = features.flatten()
+  normalized_features = flattened_features / norm(flattened_features)
+  return normalized_features
+
 model2 = ResNet50(weights='imagenet',
                          include_top=False,
                          input_shape=(512, 512, 3),
@@ -33,40 +50,23 @@ model2 = ResNet50(weights='imagenet',
 model = tf.keras.models.load_model('best_worldview_2.h5')
 model.summary()
 
-def extract_model_till_layer(model, layerNo):
-  outputs = model.layers[layerNo].output
-  model = Model(inputs=model.inputs, outputs=outputs)
-  return model
+
 model1=extract_model_till_layer(model,10)
                       
 features=[]
 try:
     
-    baseUrl="https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/MODIS_Terra_CorrectedReflectance_TrueColor/default/2012-07-09/250m/8/29/0.jpg"
-    def extract_features(url, model):
-        
-       
-        
-        a = np.asarray(PIL.Image.open('C://Users//abhig//level8images//'+url))
-       
-        a=np.resize(a,(1,224,224,3))
-        a=a/255       
-        #preprocessed_img = preprocess_input(a)
-        features = model.predict(a)
-        flattened_features = features.flatten()
-        normalized_features = flattened_features / norm(flattened_features)
-        return normalized_features
-
+   
+    
     counter=1
     for filename in os.listdir("C://Users//abhig//level8images"):
-            a=extract_features(filename, model1)
-            print("length",len(a))
-            features.append(a)
+            feats=extract_features(filename, model1)
+            print("length",len(fests))
+            features.append(feats)
             print(counter)
             counter+=1
        
-    print(len(features[0]))
-    print(features[0])
+    
 
 
      # Length of item vector that will be indexed
@@ -75,15 +75,15 @@ try:
         feature = features[p]
         t.add_item(p, feature)
 
-    t.build(40)  # 50 trees
+    t.build(40)  # 40 trees
     t.save('fullDatabase.ann')
 
 except:
-    print("aaaa")
+    
     t=AnnoyIndex(features[0])
     for p in range(len(features)):
         feature = features[p]
         t.add_item(p, feature)
 
-    t.build(40)  # 50 trees
+    t.build(40)  # 40 trees
     t.save('fullDatabase.ann')
